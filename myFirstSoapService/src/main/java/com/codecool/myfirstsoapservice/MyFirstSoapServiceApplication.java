681e28db854a27674937bde76fffdbe15af153f4
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-11-11 11:24:12 +0100

    create starter project with dependencies

---
+++ myFirstSoapService/src/main/java/com/codecool/myfirstsoapservice/MyFirstSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.myfirstsoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSoapServiceApplication.class, args);
+	}
+
+}
